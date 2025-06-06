cd21bdff6e3407124f6bc825be47eb0793786c94
NaaraLarcher <ignacio.najar2310@gmail.com>
2025-06-06 16:30:49 -0300

    Se finalizó el examen numero 2

--- Desempeño2-IgnacioNajar/src/Examen.java
+++ Desempeño2-IgnacioNajar/src/Examen.java
@@ -14,24 +14,90 @@ public class Examen {
 	// 1 => Insertar elementos en el arbol
 	public void insertar(int info) {
 		// COMPLETAR ESTE METODO
-		
+		Nodo nodoNuevo = new Nodo();
+		nodoNuevo.info = info;
+		nodoNuevo.der = null;
+		nodoNuevo.izq = null;
+
+		if (raiz == null) {
+			raiz = nodoNuevo;
+		} else {
+			Nodo anterior = null;
+			Nodo recorrer = raiz;
+
+			while (recorrer != null) {
+				anterior = recorrer;
+
+				recorrer = (info < recorrer.info) ? recorrer.izq : recorrer.der;
+				/*
+				if (info < recorrer.info) {
+					recorrer = recorrer.izq;
+				} else {
+					recorrer = recorrer.der;
+				} */
+			}
+
+			if (info < anterior.info) {
+				anterior.izq = nodoNuevo;
+			} else {
+				anterior.der = nodoNuevo;
+			}
+		}
 	}
 
 	// 2 => Encuentra el menor valor del subArbol derecho
 	public void menorValorDerecha() {
 		// COMPLETAR ESTE METODO
+		if (raiz == null || raiz.der == null) {
+			System.out.println("No existe el subarbol derecho");
+			return;
+		}
 
+		Nodo reco = raiz.der;
+		while (reco.izq != null) {
+			reco = reco.izq;
+		}
+
+		System.out.println(reco.info);
 	}
 
 	// 3 => Encuentra el mayor valor del subArbol izquierdo
 	public void mayorValorIzquierda() {
 		// COMPLETAR ESTE METODO
+		if (raiz == null || raiz.izq == null) {
+			System.out.println("No existe subarbol izquierdo");
+			return;
+		}
 
+		Nodo reco = raiz.izq;
+		while (reco.der != null) {
+			reco = reco.der;
+		}
+
+		System.out.println(reco.info);
 	}
 
 	// 4 => Buscar un valor e informar el nivel
 	public void existeValorEnNivel(int info) {
 		// COMPLETAR ESTE METODO
+		Nodo reco = raiz;
+		int nivel = 0;
+
+		while (reco != null) {
+			nivel++;
+			if (info == reco.info) {
+				System.out.println("El nodo con el valor " + info + " existe en el nivel: " + nivel);
+				return;
+			}
+				
+			if (info > reco.info) {
+				reco = reco.der;
+			} else {
+				reco = reco.izq;
+			}
+		}
+
+		System.out.println("El nodo con el valor " + info + " no existe");
 		
 	}
 
@@ -39,15 +105,22 @@ public class Examen {
     // 5 => Calcular el total de nodos hoja del arbol
     private void totalNodosHoja(Nodo reco) {	
         // COMPLETAR ESTE METODO
-		
+		if (reco != null) {
+			if (reco.izq == null && reco.der == null) {
+				cant++;
+			}
+			
+			totalNodosHoja(reco.izq);
+			totalNodosHoja(reco.der);
+		}
 	}
 
     public int totalNodosHoja() {
         cant = 0;
-	    // COMPLETAR ESTE METODO
-		
+		totalNodosHoja(raiz);
+		return cant;
+	    // COMPLETAR ESTE METODO		
 	}
-
 
 	private void imprimirEntre(Nodo reco) {
 		if (reco != null) {
@@ -69,20 +142,21 @@ public class Examen {
 		for (int x : valores) {
 			abo.insertar(x);
 		}
-
+		
 		System.out.println("1 = Impresion entreorden: ");
 		abo.imprimirEntre();
-
-		System.out.println("2 = menor valor del subArbol derecho: ");
+		
+		System.out.print("\n2 = menor valor del subArbol derecho: ");
 		abo.menorValorDerecha();
 		
-		System.out.println("3 = mayor valor del subArbol izquierdo: ");
+		
+		System.out.print("\n3 = mayor valor del subArbol izquierdo: ");
 		abo.mayorValorIzquierda();
-
-		System.out.println("4 = Buscar valor en el arbol e informar en que nivel existe: ");
+		
+		System.out.println("\n4 = Buscar valor en el arbol e informar en que nivel existe: ");
 		abo.existeValorEnNivel(100);
 		abo.existeValorEnNivel(45);
-
-        System.out.println("5 = Total de nodos hoja en el arbol = " + abo.totalNodosHoja());
+		
+        System.out.println("\n5 = Total de nodos hoja en el arbol = " + abo.totalNodosHoja());
 	}
 }
